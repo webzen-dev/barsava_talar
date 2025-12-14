6332ea62698897e86f34f0dd64ac5b3b4581b1b8
kevin <webzen.ir@gmail.com>
2025-12-15 01:49:48 +0330

    style(main):change background color for not-found page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,18 +1,19 @@
 import Link from "next/link";
 
-function NotFound (){
-
-    return(
-        <div className="bg-white flex items-center justify-center flex-col w-full h-[100vh]">
-            <div className="flex-col flex text-center gap-5" >
-                <h2 className="text-9xl text-[#dec39a]">404</h2>
-                <b className="text-2xl text-gray-600">{"(:"} صفحه ی مورد نظر یافت نشد </b>
-                <Link href={'/'} className="text-gray-400 ">بازگشت به خانه</Link>
-                <span className="loading loading-infinity loading-xl"></span>
-            </div>
-        </div>
-    )
+export default function NotFound() {
+  return (
+    <div className="bg-[var(--page-background)] flex items-center justify-center flex-col w-full h-screen">
+      <div className="flex-col flex text-center gap-5">
+        <h2 className="text-9xl text-[var(--gold)]">404</h2>
+        <b className="text-2xl text-black/70">
+          {"(:"} صفحه ی مورد نظر یافت نشد{" "}
+        </b>
+        <Link href={"/"} className="text-gray-400 ">
+          بازگشت به خانه
+        </Link>
+        
+        <span className="loading loading-infinity loading-xl"></span>
+      </div>
+    </div>
+  );
 }
-
-
-export default NotFound;
